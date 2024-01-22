97860084640881aadd2bdb15189e9e853d97fcc0
jsalasl <jsalasl@findep.com>
2024-01-22 15:55:31 -0600

    rutas basicas configuradas

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,35 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screens.dart';
 
 class HomeScreen extends StatelessWidget {
-   
   const HomeScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter + Material 3'),
-      ),
-      body: _HomeView()
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter + Material 3'),
+        ),
+        body: _HomeView());
   }
 }
 
 class _HomeView extends StatelessWidget {
-  
-
   @override
   Widget build(BuildContext context) {
     appMenuItems.length;
     return ListView.builder(
-      itemCount: appMenuItems.length,
-      itemBuilder: (context,index){
-        final menuItem = appMenuItems[index];
-        return _CustomListTile(menuItem: menuItem);
-      }
-    
-    );
+        itemCount: appMenuItems.length,
+        itemBuilder: (context, index) {
+          final menuItem = appMenuItems[index];
+          return _CustomListTile(menuItem: menuItem);
+        });
   }
 }
 
@@ -42,15 +37,21 @@ class _CustomListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final colors = Theme.of(context).colorScheme;
 
     return ListTile(
-      leading: Icon(menuItem.icon,color: colors.primary),
-      trailing: Icon(Icons.arrow_forward_ios_rounded,color: colors.primary),
+      leading: Icon(menuItem.icon, color: colors.primary),
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subTitle),
-      onTap: (){},
-      );
+      onTap: () {
+        // Navigator.of(context).push(
+        //   MaterialPageRoute(
+        //     builder: (context) => const ButtonsScreen(),
+        //   ),
+        // );
+        Navigator.pushNamed(context, menuItem.link);
+      },
+    );
   }
 }
